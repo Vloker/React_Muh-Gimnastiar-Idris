fd1bf988a80d9b13f13173848c29640c24cd628e
unknown <agimgimnastiar09@gmail.com>
2023-09-07 15:58:38 +0800

    clean code

--- 08_Clean-Code/Praktikum/js/script-code.js
+++ 08_Clean-Code/Praktikum/js/script-code.js
@@ -2,73 +2,73 @@
 const form = document.querySelector('.form');
 const data = document.querySelector('#tabelData tbody');
 
-let noUrut = 1;
+let nomorUrut = 1;
 
 form.addEventListener('submit',(e) => {
     e.preventDefault();
 
-    const pname = document.getElementById("pname").value;
-    const pcategory = document.getElementById("pcategory").value;
-    const iproduct = document.getElementById("iproduct").value;
-    const radioValue = document.querySelector('input[name="flexRadioDefault"]:checked');
-    const ADes = document.getElementById("ADes").value;
-    const pprice = document.getElementById("pprice").value;
+    const productName = document.getElementById("pname").value;
+    const productCategory = document.getElementById("pcategory").value;
+    const imageProduct = document.getElementById("iproduct").value;
+    const productFreshness = document.querySelector('input[name="flexRadioDefault"]:checked');
+    const additionalDescription = document.getElementById("ADes").value;
+    const productPrice = document.getElementById("pprice").value;
 
     // Validasi input
-    if (pname.trim() === '') {
+    if (productName.trim() === '') {
         alert('Product name tidak boleh kosong.');
         return;
-    }else if(pname.length > 25){
+    }else if(productName.length > 25){
         alert('Last Name must not exceed 25 characters')
         return false;
-    }else if (!pname.match(/^[a-zA-Z0-9\s]{1,25}$/)){
+    }else if (!productName.match(/^[a-zA-Z0-9\s]{1,25}$/)){
         alert('Name must not contain symbols')
         return false;
     }
 
-    if (pcategory.trim() === '') {
+    if (productCategory.trim() === '') {
         alert('Product category tidak boleh kosong.');
         return;
     }
 
-    if (!iproduct.trim()) {
-        const iproductInput = document.getElementById("iproduct");
-        iproductInput.value = "";
+    if (!imageProduct.trim()) {
+        const imageProductInput = document.getElementById("iproduct");
+        imageProductInput.value = "";
     }
 
-    if (!radioValue) {
+    if (!productFreshness) {
         alert('Pilih salah satu product freshness.');
         return;
     }
 
-    if (ADes.trim() === '') {
+    if (additionalDescription.trim() === '') {
         alert('Additional description tidak boleh kosong.');
         return;
     }
 
-    if (pprice.trim() === '' || isNaN(pprice)) {
+    if (productPrice.trim() === '' || isNaN(productPrice)) {
         alert('Product price harus diisi dengan angka.');
         return;
     }
 
-    // Penginputan data ke dalam tabel
+    // Menambahkan baris ke dalam tabel dari setiap inputan data yang masuk
         const newRow = data.insertRow();
 
-        const urut = newRow.insertCell(0);
-        const pnameCell = newRow.insertCell(1);
-        const pcategoryCell = newRow.insertCell(2);
-        const iproductCell = newRow.insertCell(3);
-        const radioCell = newRow.insertCell(4);
-        const ADesCell = newRow.insertCell(5);
-        const ppriceCell = newRow.insertCell(6);
+        const nomorTabel = newRow.insertCell(0);
+        const productNameCell = newRow.insertCell(1);
+        const productCategoryCell = newRow.insertCell(2);
+        const imageProductCell = newRow.insertCell(3);
+        const productFreshnessCell = newRow.insertCell(4);
+        const additionalDescriptionDesCell = newRow.insertCell(5);
+        const productPriceCell = newRow.insertCell(6);
 
-        urut.innerHTML = noUrut++;
-        pnameCell.innerHTML = pname;
-        pcategoryCell.innerHTML = pcategory;
-        iproductCell.innerHTML = iproduct;
-        radioCell.innerHTML = radioValue.value;
-        ADesCell.innerHTML = ADes;
-        ppriceCell.innerHTML = '$' + pprice;
+        nomorTabel.innerHTML = nomorUrut++;
+        productNameCell.innerHTML = productName;
+        productCategoryCell.innerHTML = productCategory;
+        imageProductCell.innerHTML = imageProduct;
+        productFreshnessCell.innerHTML = productFreshness.value;
+        additionalDescriptionDesCell.innerHTML = additionalDescription;
+        productPriceCell.innerHTML = '$' + productPrice;
         
         alert('Product berhasil ditambahkan.');
 
@@ -76,26 +76,26 @@ form.addEventListener('submit',(e) => {
 
 const rows = data.getElementsByTagName('tr')
 
-// Pencarian data dari tabel
+// Pencarian data dari dalam tabel
 const search = document.getElementById('search');
 
 search.addEventListener('click', () => {
     const kataKunci = document.getElementById('cari').value.toLowerCase()
 
 // Kondisi saat pencarian data
-    Array.from(rows).forEach((e) => {
-        const pname = e.getElementsByTagName('td')[1].textContent.toLowerCase()
-        if (pname.includes(kataKunci)) {
-            e.style.display = '';
+    Array.from(rows).forEach((elemen) => {
+        const productname = elemen.getElementsByTagName('td')[1].textContent.toLowerCase()
+        if (productname.includes(kataKunci)) {
+            elemen.style.display = '';
         } else {
-            e.style.display = 'none';
+            elemen.style.display = 'none';
         }
     });
 
 })
 
 
-// Penghapusan data Dalam tabel
+// Penghapusan data dalam tabel
 const deleteButton = document.getElementById('deleted');
 
 deleteButton.addEventListener('click', () => {
@@ -103,7 +103,7 @@ deleteButton.addEventListener('click', () => {
 // Kondisi Saat Menghapus setiap data yang terakhir di input dan nomor urut akan diperbarui
     if (rows.length > 0) {
         data.deleteRow(rows.length - 1); 
-        noUrut--;
+        nomorUrut--;
     }
 });
 
